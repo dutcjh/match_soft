e049d860beecde569ba4912a96dfc23c32ad1250
dutcjh <dutchenjianhui@163.com>
2016-03-31 22:00:54 +0800

    over frequency

--- match.m
+++ match.m
@@ -31,6 +31,9 @@ imshow(double(img_src)./255);%用imread读入图片，并用imshow在axes_topo上显示
 setappdata(handles.match,'Freq','400k');
 setappdata(handles.match,'RL','3300');
 setappdata(handles.match,'Req','330k');
+setappdata(handles.match,'Ind','10m');
+setappdata(handles.match,'Cap','12u');
+setappdata(handles.match,'Hvg','10');
 
 % --- Outputs from this function are returned to the command line.
 function varargout = match_OutputFcn(hObject, eventdata, handles) 
@@ -94,6 +97,15 @@ end
 
 
 function ind1_Callback(hObject, eventdata, handles)
+str = get(hObject,'String');
+value = str2value(str);
+if value < 0 % 判断输入是否有效 无效输入
+    str = getappdata(handles.match,'Ind');
+    set(hObject,'String',str);
+else
+    setappdata(handles.match,'Ind',str);
+end
+
 
 % --- Executes during object creation, after setting all properties.
 function ind1_CreateFcn(hObject, eventdata, handles)
@@ -104,10 +116,34 @@ end
 
 
 function cap1_Callback(hObject, eventdata, handles)
-
+str = get(hObject,'String');
+value = str2value(str);
+if value < 0 % 判断输入是否有效 无效输入
+    str = getappdata(handles.match,'Cap');
+    set(hObject,'String',str);
+else
+    setappdata(handles.match,'Cap',str);
+end
 
 % --- Executes during object creation, after setting all properties.
 function cap1_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function Hvol_Callback(hObject, eventdata, handles)
+str = get(hObject,'String');
+value = str2value(str);
+if value < 0 % 判断输入是否有效 无效输入
+    str = getappdata(handles.match,'Hvg');
+    set(hObject,'String',str);
+else
+    setappdata(handles.match,'Hvg',str);
+end
+
+% --- Executes during object creation, after setting all properties.
+function Hvol_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -118,39 +154,39 @@ function solve_Callback(hObject, eventdata, handles)
 val = get(handles.pop1,'Value');
 switch val
     case 1
-        boost(handles);
+        boost(handles,1);
     case 2
-        buck(handles);
+        buck(handles,1);
     case 3
-        T_xing(handles);
+        T_xing(handles,1);
     case 4
-        Pi_xing(handles);
+        Pi_xing(handles,1);
 end
 
 function value = str2value(str)
+unit_map = containers.Map({'M','k','K','m','u','n','p'},...
+    {1e6,1e3,1e3,1e-3,1e-6,1e-9,1e-12});
 strformat = '%f%c';
 [num,k] = sscanf(str,strformat);
 if k == 1
     value = num;
 else
-    switch num(2)
-        case {107,75} % k K
-            value = num(1)*1e3;
-        case 77 % M
-            value = num(1)*1e6;
-        otherwise 
-            value = num(1);
-    end         
+    if isKey(unit_map,char(num(2)))
+        value = num(1) * unit_map(char(num(2)));
+    else 
+        value = num(1);
+    end
 end
 
-
-function Hvol_Callback(hObject, eventdata, handles)
-
-
-% --- Executes during object creation, after setting all properties.
-function Hvol_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
+function unitstr = value2str(value)
+unitpnum = [1e6,1e3,1,1e-3,1e-6,1e-9,1e-12];
+unitpstr = ['M','k',' ','m','u','n','p'];
+unitstr = '0';
+for i = 1:length(unitpnum)
+    if value >= unitpnum(i)
+        unitstr = strcat(num2str(value/unitpnum(i)),unitpstr(i));
+        break;
+    end
 end
 
 
@@ -182,23 +218,41 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-function boost(handles)
-freq = str2value(get(handles.freq,'String'));
-RL = str2value(get(handles.res_load,'String'));
-Req = str2value(get(handles.res_equ,'String'));
-if Req <= RL
-    w = 2*pi*freq;
-    L1 = Req*sqrt(RL/Req-1)/w;
-    C1 = sqrt(RL/Req-1)/(w*RL);
-    Hv = sqrt(RL/Req);
-else
-    L1 = 0;
-    C1 = 0;
-    Hv = 1;
+function boost(handles,flag)
+if flag == 1
+    freq = str2value(get(handles.freq,'String'));
+    Hv = str2value(get(handles.Hvol,'String'));
+    Req = str2value(get(handles.res_equ,'String'));
+    RL = Req*Hv^2;
+    if Hv > 1
+        w = 2*pi*freq;
+        L1 = Req*sqrt(RL/Req-1)/w;
+        C1 = sqrt(RL/Req-1)/(w*RL);
+    else
+        L1 = 0;
+        C1 = 0;
+    end
+    set(handles.ind1,'String',value2str(L1));
+    set(handles.cap1,'String',value2str(C1));
+    set(handles.res_load,'String',value2str(RL));
+else 
+    L1 = str2value(get(handles.ind1,'String'));
+    C1 = str2value(get(handles.cap1,'String'));
+    RL = str2value(get(handles.res_load,'String'));
+    if (C1*RL^2)/L1 > 1
+        w = sqrt((C1*RL^2)/L1-1)/(RL*C1);
+        freq = w/(2*pi);
+        Hv = sqrt((w*RL*C1)^2+1);
+        Req = RL/((w*RL*C1)^2+1);
+    else 
+        freq = 0;
+        Hv = 1;
+        Req =0;
+    end
+    set(handles.freq,'String',value2str(freq));
+    set(handles.Hvol,'String',num2str(Hv));
+    set(handles.res_equ,'String',value2str(Req));
 end
-set(handles.ind1,'String',num2str(L1*1e3));
-set(handles.cap1,'String',num2str(C1*1e12));
-set(handles.Hvol,'String',num2str(Hv));
 
 w_start = floor(log10(freq)/2);
 w_end = ceil(log10(freq))+2;
@@ -217,24 +271,41 @@ xlabel('f/Hz');
 ylabel('φ_{L}');
 
 
-function buck(handles)
-freq = str2value(get(handles.freq,'String'));
-RL = str2value(get(handles.res_load,'String'));
-Req = str2value(get(handles.res_equ,'String'));
-if Req >= RL
-w = 2*pi*freq;
-L1 = RL*sqrt(Req/RL-1)/w;
-C1 = L1/(Req*RL);
-Hv = sqrt(RL/Req);
-set(handles.res_equ,'String',num2str(L1/(RL*C1)));
+function buck(handles,flag)
+if flag == 1
+    freq = str2value(get(handles.freq,'String'));
+    Hv = str2value(get(handles.Hvol,'String'));
+    Req = str2value(get(handles.res_equ,'String'));
+    RL = Req*Hv^2;
+    if Hv < 1
+        w = 2*pi*freq;
+        L1 = RL*sqrt(Req/RL-1)/w;
+        C1 = L1/(Req*RL);
+    else
+        L1 = 0;
+        C1 = 0;
+    end
+    set(handles.ind1,'String',value2str(L1));
+    set(handles.cap1,'String',value2str(C1));
+    set(handles.res_load,'String',value2str(RL));
 else
-L1 = 0;
-C1 = 0;
-Hv = 1;
+    L1 = str2value(get(handles.ind1,'String'));
+    C1 = str2value(get(handles.cap1,'String'));
+    RL = str2value(get(handles.res_load,'String'));
+    if (1/(L1*C1)-(RL/L1)^2) > 0
+        w = sqrt(1/(L1*C1)-(RL/L1)^2);
+        freq = w/(2*pi);
+        Hv = RL*sqrt(C1/L1);
+        Req = L1/(RL*C1);
+    else
+        freq = 0;
+        Hv = 1;
+        Req =0;
+    end
+    set(handles.freq,'String',value2str(freq));
+    set(handles.Hvol,'String',num2str(Hv));
+    set(handles.res_equ,'String',value2str(Req));
 end
-set(handles.ind1,'String',num2str(L1*1e3));
-set(handles.cap1,'String',num2str(C1*1e12));
-set(handles.Hvol,'String',num2str(Hv));
 
 w_start = floor(log10(freq)/2);
 w_end = ceil(log10(freq))+2;
@@ -252,18 +323,32 @@ grid on;
 xlabel('f/Hz');
 ylabel('φ_{L}');
 
-function T_xing(handles)
-freq = str2value(get(handles.freq,'String'));
-RL = str2value(get(handles.res_load,'String'));
-Req = str2value(get(handles.res_equ,'String'));
-w = 2*pi*freq;
-L1 = sqrt(RL*Req)/w;
-C1 = 1/(sqrt(RL*Req)*w);
-Hv = sqrt(RL/Req);
-set(handles.ind1,'String',num2str(L1*1e3));
-set(handles.cap1,'String',num2str(C1*1e12));
-set(handles.Hvol,'String',num2str(Hv));
+function T_xing(handles,flag)
+if flag == 1
+    freq = str2value(get(handles.freq,'String'));
+    Hv = str2value(get(handles.Hvol,'String'));
+    Req = str2value(get(handles.res_equ,'String'));
+    RL = Req*Hv^2;
+    w = 2*pi*freq;
+    L1 = sqrt(RL*Req)/w;
+    C1 = 1/(sqrt(RL*Req)*w);
+    set(handles.ind1,'String',value2str(L1));
+    set(handles.cap1,'String',value2str(C1));
+    set(handles.res_load,'String',value2str(RL));
+else
+    L1 = str2value(get(handles.ind1,'String'));
+    C1 = str2value(get(handles.cap1,'String'));
+    RL = str2value(get(handles.res_load,'String'));
+    
+    w = sqrt(1/(L1*C1));
+    freq = w/(2*pi);
+    Hv = RL*sqrt(C1/L1);
+    Req = L1/(RL*C1);
 
+    set(handles.freq,'String',value2str(freq));
+    set(handles.Hvol,'String',num2str(Hv));
+    set(handles.res_equ,'String',value2str(Req));
+end
 w_start = floor(log10(freq)/2);
 w_end = ceil(log10(freq))+2;
 f = logspace(w_start,w_end,200);
@@ -280,17 +365,32 @@ grid on;
 xlabel('f/Hz');
 ylabel('φ_{L}');
 
-function Pi_xing(handles)
-freq = str2value(get(handles.freq,'String'));
-RL = str2value(get(handles.res_load,'String'));
-Req = str2value(get(handles.res_equ,'String'));
-w = 2*pi*freq;
-L1 = sqrt(RL*Req)/w;
-C1 = 1/(sqrt(RL*Req)*w);
-Hv = sqrt(RL/Req);
-set(handles.ind1,'String',num2str(L1*1e3));
-set(handles.cap1,'String',num2str(C1*1e12));
-set(handles.Hvol,'String',num2str(Hv));
+function Pi_xing(handles,flag)
+if flag == 1
+    freq = str2value(get(handles.freq,'String'));
+    Hv = str2value(get(handles.Hvol,'String'));
+    Req = str2value(get(handles.res_equ,'String'));
+    RL = Req*Hv^2;
+    w = 2*pi*freq;
+    L1 = sqrt(RL*Req)/w;
+    C1 = 1/(sqrt(RL*Req)*w);
+    set(handles.ind1,'String',value2str(L1));
+    set(handles.cap1,'String',value2str(C1));
+    set(handles.res_load,'String',value2str(RL));
+else
+    L1 = str2value(get(handles.ind1,'String'));
+    C1 = str2value(get(handles.cap1,'String'));
+    RL = str2value(get(handles.res_load,'String'));
+    
+    w = sqrt(1/(L1*C1));
+    freq = w/(2*pi);
+    Hv = RL*sqrt(C1/L1);
+    Req = L1/(RL*C1);
+
+    set(handles.freq,'String',value2str(freq));
+    set(handles.Hvol,'String',num2str(Hv));
+    set(handles.res_equ,'String',value2str(Req));
+end
 
 w_start = floor(log10(freq)/2);
 w_end = ceil(log10(freq))+2;
@@ -307,3 +407,18 @@ semilogx(f,angle(H).*180./pi,'Linewidth',2,'Color','r');
 grid on;
 xlabel('f/Hz');
 ylabel('φ_{L}');
+
+
+% --- Executes on button press in resolve.
+function resolve_Callback(hObject, eventdata, handles)
+val = get(handles.pop1,'Value');
+switch val
+    case 1
+        boost(handles,2);
+    case 2
+        buck(handles,2);
+    case 3
+        T_xing(handles,2);
+    case 4
+        Pi_xing(handles,2);
+end
